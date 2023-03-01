7f933a0edc11a1ee208218e45e9ee793914d6f20
nicholsons <sarah.sanders@ucl.ac.uk>
2023-03-01 09:51:44 +0000

    change fixture name

--- tests/tests_paralympic_app/test_para_front_end_windows.py
+++ tests/tests_paralympic_app/test_para_front_end_windows.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 @pytest.fixture(scope="module")
-def flask_port():
+def flask_port_para():
     """Ask OS for a free port."""
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind(("", 0))
@@ -19,7 +19,7 @@ def flask_port():
 
 
 @pytest.fixture(scope="module")
-def run_app_win(flask_port):
+def run_app_win(flask_port_para):
     """Runs the Flask app for live server testing on Windows"""
     server = subprocess.Popen(
         [
@@ -28,7 +28,7 @@ def run_app_win(flask_port):
             "paralympic_app:create_app('paralympic_app.config.TestConfig')",
             "run",
             "--port",
-            str(flask_port),
+            str(flask_port_para),
         ]
     )
     sleep(5)
@@ -38,18 +38,18 @@ def run_app_win(flask_port):
         server.terminate()
 
 
-def test_home_page_running(flask_port):
+def test_home_page_running(flask_port_para):
     """
     GIVEN a running app
     WHEN the homepage is accessed successfully
     THEN the status code will be 200
     """
-    url = f"http://localhost:{flask_port}"
+    url = f"http://localhost:{flask_port_para}"
     response = requests.get(url)
     assert response.status_code == 200
 
 
-def test_event_detail_page_selected(chrome_driver, flask_port):
+def test_event_detail_page_selected(chrome_driver, flask_port_para):
     """
     GIVEN a running app
     WHEN the homepage is accessed
@@ -58,7 +58,7 @@ def test_event_detail_page_selected(chrome_driver, flask_port):
     AND the page should contain an element with the id "highlights"
     should be displayed and contain a text value "First Games"
     """
-    url = f"http://localhost:{flask_port}"
+    url = f"http://localhost:{flask_port_para}"
     chrome_driver.get(url)
     # Wait until the element with id="1" is on the page
     # https://www.selenium.dev/documentation/webdriver/waits/ and then click on it
@@ -71,7 +71,7 @@ def test_event_detail_page_selected(chrome_driver, flask_port):
     assert "First Games" in text
 
 
-def test_home_nav_link_returns_home(chrome_driver, flask_port):
+def test_home_nav_link_returns_home(chrome_driver, flask_port_para):
     """
     GIVEN a running app
     WHEN the homepage is accessed
@@ -79,7 +79,7 @@ def test_home_nav_link_returns_home(chrome_driver, flask_port):
     AND then the user clicks on the navbar in the 'Home' link
     THEN the page url should be "http://127.0.0.1:5000/"
     """
-    url = f"http://localhost:{flask_port}"
+    url = f"http://localhost:{flask_port_para}"
     chrome_driver.get(url)
     # Wait until the element with id="1" is on the page
     # https://www.selenium.dev/documentation/webdriver/waits/
